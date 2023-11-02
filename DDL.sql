1acf12f14f00bcca78e8dcea7a9fd795bf1aeddc
hoowave <ma5ter@naver.com>
2023-11-02 19:52:41 +0900

    DDL 추가

---
+++ DDL.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `tbl_members` (
+                               `id` bigint NOT NULL AUTO_INCREMENT comment '관리번호',
+                               `member_token` varchar(30) NOT NULL comment '식별자',
+                               `name` varchar(10) NOT NULL comment '이름',
+                               `phone` varchar(15) NOT NULL comment '전화번호',
+                               `password` varchar(100) NOT NULL comment '비밀번호',
+                               `nickname` varchar(20) comment '닉네임',
+                               `status` varchar(8) NOT NULL comment '계정상태',
+                               `created_at` datetime(6) not null comment '생성 일시',
+                               `updated_at` datetime(6) null comment '수정 일시',
+                               PRIMARY KEY (`id`)
+) comment '회원정보 테이블' charset = utf8mb4;
+
+CREATE TABLE `tbl_terms` (
+                             `id` bigint NOT NULL AUTO_INCREMENT comment '관리번호',
+                             `member_token` varchar(30) NOT NULL comment '식별자',
+                             `peristalsis` varchar(1) NOT NULL comment '예약 앱에 연동하기',
+                             `privacy_offer_agree` varchar(1) NOT NULL comment '개인정보 제공 동의',
+                             `push_notify` varchar(1) NOT NULL comment '푸시 알림',
+                             `sms_notify` varchar(1) NOT NULL comment 'SMS 수신',
+                             `email_notify` varchar(1) NOT NULL comment '이메일 수신',
+                             PRIMARY KEY (`id`)
+) comment '약관 테이블' charset = utf8mb4;
