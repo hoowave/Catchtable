69e1a957496303bbe9077945829b2c9ed6304dbb
hoowave <ma5ter@naver.com>
2023-11-06 17:21:50 +0900

    20231106

--- DDL.sql
+++ DDL.sql
@@ -21,3 +21,27 @@ CREATE TABLE `tbl_terms` (
                              `email_notify` varchar(1) NOT NULL comment '이메일 수신',
                              PRIMARY KEY (`id`)
 ) comment '약관 테이블' charset = utf8mb4;
+
+CREATE TABLE `tbl_shop` (
+                            `id` bigint NOT NULL AUTO_INCREMENT comment '관리번호',
+                            `shop_token` varchar(30) NOT NULL comment '식별자',
+                            `shop_id` varchar(20) NOT NULL comment '사장님 아이디',
+                            `shop_pw` varchar(100) NOT NULL comment '사장님 비밀번호',
+                            `category` varchar(20) NOT NULL comment '카테고리',
+                            `name` varchar(20) NOT NULL comment '가게이름',
+                            `introduction` varchar(200) comment '가게소개',
+                            `created_at` datetime(6) not null comment '생성 일시',
+                            PRIMARY KEY (`id`)
+) comment '가게정보 테이블' charset = utf8mb4;
+
+CREATE TABLE `tbl_shop_information` (
+                                        `id` bigint NOT NULL AUTO_INCREMENT comment '관리번호',
+                                        `shop_token` varchar(30) NOT NULL comment '식별자',
+                                        `phone` varchar(15) comment'가게 전화번호',
+                                        `info_introduction` varchar(300) comment '매장소개',
+                                        `opening` varchar(300) comment '영업시간',
+                                        `holiday` varchar(300) comment '정기휴무',
+                                        `instruction` varchar(300) comment '안내 및 유의사항',
+                                        `homepage` varchar(300) comment '홈페이지',
+                                        PRIMARY KEY (`id`)
+) comment '가게 상세정보 테이블' charset = utf8mb4;
